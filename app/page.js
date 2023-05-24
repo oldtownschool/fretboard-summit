d7af74d817e2f9ce397da328f0bc70b1c07c351a
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2023-05-24 13:29:27 -0500

    added homepage placeholder video img

--- app/page.js
+++ app/page.js
@@ -5,10 +5,16 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gradient-to-r from-[#322D35] to-[#4C454F] ">
-        <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex mt-4">
-          homepage
+      <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-r from-[#322D35] to-[#4C454F]">
+        <div className="w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <Image
+            src="/DSC07226.png"
+            alt="fs video placeholder"
+            width={1200}
+            height={500}
+          />
         </div>
+
         {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             Get started by editing&nbsp;
