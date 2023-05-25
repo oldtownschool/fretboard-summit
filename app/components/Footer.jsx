bd7d8a77de2f40a37d34282a92e9d8797342051c
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2023-05-25 17:42:07 -0500

    added sponsors. need to add the social and bottom nav links tomorrow

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,5 +4,230 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => {
-  return <div className="w-full h-48 bg-[#F0E5D1]">footer</div>;
+  return (
+    <div className="w-full bg-[#F0E5D1] py-8">
+      <div className="flex flex-col">
+        <div className="flex justify-center">
+          <Image
+            src="/Group25.png"
+            width={250}
+            height={32}
+            alt="Fretboard Journal Guitar Pick Logo"
+          />
+        </div>
+        <div className="flex justify-center pt-4">
+          <Image
+            className="hidden md:flex"
+            src="/borderRed1.png"
+            alt="border"
+            width={200}
+            height={5}
+            style={{ alignSelf: "center" }}
+          />
+          <h4 className="text-3xl tracking-wider	uppercase text-[#BE4338] font-handbill font-normal md:mx-4">
+            Fretboard summit is brought to you by
+          </h4>
+          <Image
+            className="hidden md:flex"
+            src="/borderRed1.png"
+            alt="border"
+            width={200}
+            height={5}
+            style={{ alignSelf: "center" }}
+          />
+        </div>
+        <div className="flex justify-center pt-4">
+          <Image
+            className="flex mx-2"
+            src="/FJlogo.png"
+            alt="border"
+            width={250}
+            height={150}
+            style={{ alignSelf: "center" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/OTS_2L_left_horz_black1.png"
+            alt="border"
+            width={350}
+            height={150}
+            style={{ alignSelf: "center" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="hidden md:flex"
+            src="/borderRed2.png"
+            alt="border"
+            width={350}
+            height={5}
+            style={{ alignSelf: "center" }}
+          />
+          <h4 className="text-3xl tracking-widest	uppercase text-[#BE4338] font-handbill font-normal md:mx-4">
+            Sponsors
+          </h4>
+          <Image
+            className="hidden md:flex"
+            src="/borderRed2.png"
+            alt="border"
+            width={350}
+            height={5}
+            style={{ alignSelf: "center" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="flex mx-2"
+            src="/image5.png"
+            alt="Henriksen"
+            width={200}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image6.png"
+            alt="Martin&amp;Co"
+            width={200}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image7.png"
+            alt="Santa Cruz"
+            width={200}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="flex mx-2"
+            src="/image8.png"
+            alt="Collings Guitar"
+            width={150}
+            height={150}
+            style={{ alignSelf: "center" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image9.png"
+            alt="Yamaha"
+            width={150}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="flex mx-2"
+            src="/image10.png"
+            alt="Bourgeors"
+            width={100}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image11.png"
+            alt="Rode"
+            width={100}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image13.png"
+            alt="Galloup"
+            width={100}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/taylorlogo.png"
+            alt="Taylor"
+            width={100}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/lake-pine-logo-primary-dark-rgb-500px-w-72ppi1.png"
+            alt="Lake and Pine"
+            width={100}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="flex mx-2"
+            src="/Bruce-Guitars-Banner-WEB1.png"
+            alt="Bruce Guitars.png"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image14.png"
+            alt="Acoustic Coffee Co"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image15.png"
+            alt="D'Addario"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image12.png"
+            alt="Thompson"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image16.png"
+            alt="Pacific Rim Tonewood"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "difference" }}
+          />
+          <Image
+            className="flex mx-2 sm:ml-12"
+            src="/image17.png"
+            alt="Acoustic Tuesday"
+            width={75}
+            height={150}
+            style={{ alignSelf: "center", mixBlendMode: "multiply" }}
+          />
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Image
+            className="hidden md:flex"
+            src="/borderRedbtm.png"
+            alt="border"
+            width={850}
+            height={5}
+            style={{ alignSelf: "center" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
